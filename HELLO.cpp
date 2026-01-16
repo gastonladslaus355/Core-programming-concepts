380af6e9efe92a89bed9ebc50f924be2ebec19f1
Gaston <gastonladslaus355@gmail.com>
2026-01-16 21:38:06 +0300

    Ehe welcome to the world of programming

---
+++ HELLO.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x ;
+cout<<"Enter any number between 1-2"<<endl;
+cin>>x;
+while(x==1){
+    cout<<"You are Welcome to the World of Programming."; break;
+}
+
+    return 0;
+}
